9479ff65ad71afa3a58a56ae5a81ead9c62158db
chautsunman <tsunman.chau96@gmail.com>
2016-10-21 11:05:06 +0800

    Coding Style index (#13)
    Modify index.html, index.js, and site.js coding style
    
    - 4-space indentation is used
    - extra lines are removed

--- app/js/index.js
+++ app/js/index.js
@@ -1,32 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    $("#btn_admin").click(function() {
+        var val = $('#input_text').val();
 
-
-    $("#btn_admin").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "admin.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
+        if (val !== '') {
+            var url = "admin.html?q=" + val;
+            window.location.href = url ;
+            return false;
+        }
     });
 
-    $("#btn_leader").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "team.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
+    $("#btn_leader").click(function() {
+        var val = $('#input_text').val();
+
+        if (val !== '') {
+            var url = "team.html?q=" + val;
+            window.location.href = url ;
+            return false;
+        }
     });
 
-    $("#btn_member").click(function(){
-    	var val = $('#input_text').val();
-    	if ( val !== '' ) {
-    		var url = "member.html?q=" + val;
-    		window.location.href= url ;
-    		return false;
-    	}
+    $("#btn_member").click(function() {
+        var val = $('#input_text').val();
+
+        if (val !== '') {
+            var url = "member.html?q=" + val;
+            window.location.href = url ;
+            return false;
+        }
     });
-
-
 });
