3ac693a38e494a1910761afcb726804b7c712638
chautsunman <tsunman.chau96@gmail.com>
2016-11-01 16:16:35 +0800

    #29 facebook login (#30)
    #29 
    
    - implement Facebook authentication in the index page

--- app/js/index.js
+++ app/js/index.js
@@ -31,6 +31,50 @@ $(document).ready(function() {
 });
 
 angular.module('teamform-index-app', ['firebase', 'ngMaterial'])
-.controller('IndexCtrl', function($scope, $firebaseObject, $firebaseArray) {
+.controller('IndexCtrl', function($scope, $firebaseObject, $firebaseArray, $window) {
     initializeFirebase();
+
+
+    /* Facebook Authentication */
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    // login function
+    $scope.login = function() {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user);
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+        });
+    };
+
+    // logout function
+    $scope.logout = function() {
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+            console.log('sign-out successful');
+        }, function(error) {
+            // An error happened.
+            console.log('sign-out error');
+        });
+    };
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            console.log(user);
+        } else {
+            // No user is signed in.
+            console.log('no user is signed in');
+        }
+    });
 });
