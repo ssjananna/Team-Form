42fd1c76c62f3a5d4080f82c5e6a2ea11cb37b39
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-11-06 11:16:51 +0800

    Create the about page (#33)
    - move the about content from the index page to the new about page (about.html)
    - remove the "Fork me on GitHub" link in the index page

---
+++ app/js/about.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+});
+
+angular.module('teamform-about-app', ['firebase', 'ngMaterial'])
+.controller('AboutCtrl', function($scope, $firebaseObject, $firebaseArray) {
+    initializeFirebase();
+    $scope.team = [
+        {pic: "steven.jpg", name: "Steven To", email: "clto@ust.hk", information: "Hello!"},
+        {pic: "man.jpg", name: "Man God", email: "ManGod@ust.hk", information: "Hello!!!!!"}
+    ];
+});
