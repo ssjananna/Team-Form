b2803702b09288d6843b025976ee05c3ad5be175
Shermin ANANNA <ssjananna@connect.ust.hk>
2016-11-14 02:10:57 +0800

    Function missingSkillsMatched done

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,33 @@
+/*
+- function (input: preferred skills of a team, skills already satisfied by other members, skills of the current user,
+			output: number of missing skills matched)
+*/
+function missingSkillsMatched(preferredSkills, foundSkills, currentSkills){
+	var missingSkillsArray = preferredSkills.filter(
+	function(each){
+		for (var i=0;i<foundSkills.length;i++){
+			if(each!=foundSkills[i]){
+				return true;
+			}
+		}
+		return false;
+	}
+	);
+	var missingSkillsMatchedArray = missingSkillsArray.filter(
+	function(each2){
+		for (var i=0;i<currentSkills.length;i++){
+			if(each2==currentSkills[i]){
+				return true;
+			}
+		}
+		return false;
+	}
+	);
+	return missingSkillsMatchedArray;
+}
+
+
+
+
+	
 
--- app/unit_tests/common_test.js
+++ app/unit_tests/common_test.js
@@ -1,1 +1,31 @@
+describe('Test common_test.js', function() {
+	
+    //
+    // A test case of missingSkillsMatched
+    //
+    describe('missingSkillsMatched Coverage Test', function() {
 
+        it('returns match of missing skills of team and skills of user', function() {
+        	var team = {
+
+        		currentTeamSize: 1,
+        		size: 5,
+        		preferredSkills: ["C++", "java", "php"],
+        		teamMembers:[{name:"Shermin", uid:"blah", skills:"C++"}],
+				foundSkills: ["C++"],
+        	};
+
+        	var user ={
+				name:"Pooja",
+				uid:"meh",
+        		skills:["C++","php"],
+        	};
+
+            var missingMatched = missingSkillsMatched(team.preferredSkills, team.foundSkills, user.skills);
+            var expected = ["php"];
+            expect(matched).toEqual(expected);
+        });
+
+    });
+
+});
