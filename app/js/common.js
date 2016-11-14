63452479873498ed8d923192c51512f4c6f8e6dc
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-11-14 08:37:32 +0800

    Filter teams that still have places left (function) (#43)
    ```
    /**
    * filter teams that still have places left
    *
    * @param teams array of teams
    * @return filtered teams
    */
    ```

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,13 @@
+function isAvailable(team) {
+	return team.currentTeamSize < team.size;
+}
 
+/**
+ * filter teams that still have places left
+ *
+ * @param teams array of teams
+ * @return filtered teams
+ */
+function getAvailableTeam(teams) {
+	return teams.filter(isAvailable);
+}
--- app/unit_tests/common_test.js
+++ app/unit_tests/common_test.js
@@ -1,1 +1,30 @@
+describe('Test common_test.js', function() {
+    // A test case of getAvailableTeam
+    describe('getAvailableTeam Coverage Test', function() {
+        it('returns available teams from a list of teams', function() {
+        	var teams = [
+        	{
+        		currentTeamSize: 1,
+        		size: 5,
+        		skills: ["C++"],
+        		teamMembers: [{name:"STO", uid:"qwertyqwerty"}]
+        	},
+        	{
+        		currentTeamSize: 4,
+        		size: 4,
+        		skills: ["C++"],
+        		teamMembers: []
+        	}];
 
+            var answer = getAvailableTeam(teams);
+            var expected = [{
+				currentTeamSize: 1,
+				size: 5,
+				skills: ["C++"],
+				teamMembers: [{name:"STO", uid:"qwertyqwerty"}]
+			}];
+
+            expect(answer).toEqual(expected);
+        });
+    });
+});
