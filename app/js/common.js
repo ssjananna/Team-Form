470aa03d9c7e6d297c005e361991eb9ea315ae73
chautsunman <tsunman.chau96@gmail.com>
2016-11-15 21:23:46 +0800

    Filter teams that have skills that the user has (function) (#54)
    #66

--- app/js/common.js
+++ app/js/common.js
@@ -36,6 +36,28 @@ function missingSkillsMatched(preferredSkills, foundSkills, currentSkills){
 }
 
 
+/**
+ * calculate the number of skills matched
+ *
+ * @param preferredSkills preferred skills of a team
+ * @param currentSkills skills of the signed in user
+ * @return skills match and number of skills matched
+ */
+function isMatched(preferredSkills, currentSkills){
+	var filteredArray = preferredSkills.filter(
+	function(each){
+		for (var i=0;i<currentSkills.length;i++){
+			if(each==currentSkills[i]){
+				return true;
+			}
+		}
+		return false;
+	}
+	);
+	return {match: filteredArray, number: filteredArray.length};
+}
+
+
 function hasNoTeam(member){
 	return member.team === undefined;
 }
--- app/unit_tests/common_test.js
+++ app/unit_tests/common_test.js
@@ -57,6 +57,34 @@ describe('Test common', function() {
     });
 
 
+    // A test case of matchedSkills
+    describe('getMatchedSkills Coverage Test', function() {
+
+        it('returns matched skills of team and user', function() {
+            var team = {
+
+                currentTeamSize: 1,
+                size: 5,
+                preferredSkills: ["C++", "java", "php"],
+                teamMembers:[{name:"STO", uid:"qwertyqwerty"}]
+            };
+
+            var user ={
+                username:"user1",
+                skills:["C++","php"],
+                preferredTeams:["team1", "team5"]
+
+            };
+
+            var matched = isMatched(team.preferredSkills, user.skills);
+            var expected = ["C++","php"];
+            
+            expect(matched.match).toEqual(expected);
+            expect(matched.number).toEqual(1);
+        });
+    });
+
+
     // A test case of MembersWithNoTeam
     describe('MembersWithNoTeam Coverage Test', function() {
         it('returns members who do not have a team yet', function() {
